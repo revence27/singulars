bc0e8a10b30592b6e4095537ddad5e5eb6c73d39
Revence Kalibwani <revence@1st.ug>
2011-10-19 02:08:01 +0300

    Put seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+SingularWord.create :word => 'ethics', :sample_sentence => 'The ethics his paper introduces is truly appaling.'
+SingularWord.create :word => 'politics', :sample_sentence => 'The politics of this country is interesting indeed.'
+SingularWord.create :word => '&aelig;sthetics', :sample_sentence => 'An &aelig;sthetics well capable of explaining heavy metal music.'
+SingularWord.create :word => 'mathematics', :sample_sentence => 'Mathematics is any game that follows strict rules.'
+SingularWord.create :word => 'physics', :sample_sentence => 'Physics is the ultimate Baconian discipline.'
+SingularWord.create :word => 'metaphysics', :sample_sentence => 'Spinoza then introduced a metaphysics that begged ten questions for each one it answered.'
